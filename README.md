489411c7c7a3a35faa194e30c71d32ba50a3cbba
PhilN8 <phil.nyaga@gmail.com>
2024-10-09 00:09:49 +0300

    Cleanup for Ep.2, added new models

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # Ping Service API
 
 In here, I will write the lessons I learn from JustSteveKing's [API course](https://apiacademy.treblle.com/laravel-api-course/intro-and-setup)
+
+## Lesson 2
+
+`auth:sanctum` middleware requires the `web` routes to be specified because some session information is set up automatically via web, which are required by the Sanctum middleware
--- routes/web/routes.php
+++ routes/web/routes.php
@@ -2,13 +2,14 @@
 
 declare(strict_types=1);
 
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Auth\NewPasswordController;
+use App\Http\Controllers\Auth\VerifyEmailController;
+use App\Http\Controllers\Auth\RegisteredUserController;
+use App\Http\Controllers\Auth\PasswordResetLinkController;
 use App\Http\Controllers\Auth\AuthenticatedSessionController;
 use App\Http\Controllers\Auth\EmailVerificationNotificationController;
-use App\Http\Controllers\Auth\NewPasswordController;
-use App\Http\Controllers\Auth\PasswordResetLinkController;
-use App\Http\Controllers\Auth\RegisteredUserController;
-use App\Http\Controllers\Auth\VerifyEmailController;
-use Illuminate\Support\Facades\Route;
+
 
 Route::post('/register', [RegisteredUserController::class, 'store'])
     ->middleware('guest')
