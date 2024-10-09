e86e8e908254b99ddc20c4807fd4520560944967
PhilN8 <phil.nyaga@gmail.com>
2024-10-09 16:24:14 +0300

    Rate Limiting applied for Auth and Api

--- routes/api/routes.php
+++ routes/api/routes.php
@@ -5,16 +5,18 @@ declare(strict_types=1);
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::middleware(['auth:sanctum'])->get('/user', fn(Request $request) => $request->user());
+Route::middleware(['auth:sanctum', 'throttle:api'])->group(static function (): void {
+    Route::get('/user', fn(Request $request) => $request->user());
 
-Route::prefix('services')->as('services:')->group(base_path(
-    path: 'routes/api/services.php',
-));
+    Route::prefix('services')->as('services:')->group(base_path(
+        path: 'routes/api/services.php',
+    ))->middleware(['throttle:100,1']);
 
-Route::prefix('credentials')->as('credentials:')->group(base_path(
-    path: 'routes/api/credentials.php',
-));
+    Route::prefix('credentials')->as('credentials:')->group(base_path(
+        path: 'routes/api/credentials.php',
+    ));
 
-Route::prefix('checks')->as('checks:')->group(base_path(
-    path: 'routes/api/checks.php',
-));
+    Route::prefix('checks')->as('checks:')->group(base_path(
+        path: 'routes/api/checks.php',
+    ));
+});
